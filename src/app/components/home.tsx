1c9b91fac46f5325edc13239d136172f2e0c8141
Alexandre Dissi <alexandredissi@icloud.com>
2023-05-30 22:32:14 +0200

    modif responsive

--- src/app/components/home.tsx
+++ src/app/components/home.tsx
@@ -5,7 +5,7 @@ export default function HomePresentation() {
     return (
         <div className="flex flex-col-reverse gap-0 md:flex-col-reverse lg:flex-row text-center md:text-left md:justify-between items-center" id='home'>
             <div data-aos="fade-right">
-                <div className="text-4xl md:text-6xl font-mono font-extrabold mt-4 leading-snug text-solidheadinglight dark:text-solidheadingdark">
+                <div className="text-4xl md:text-6xl font-mono font-extrabold mt-4 leading-loose text-solidheadinglight dark:text-solidheadingdark">
 
                     <Typewriter
                         options={{
@@ -13,7 +13,7 @@ export default function HomePresentation() {
                         }}
                         onInit={(typewriter) => {
                             typewriter
-                                .typeString('Hi ,')
+                                .typeString('Hi 👋🏼,')
                                 .pauseFor(200)
                                 .typeString('<br>')
                                 .pauseFor(200)
@@ -36,7 +36,7 @@ export default function HomePresentation() {
             </div>
             <div data-aos="fade-left">
                 <div className="p-2 bg-gradient-to-l from-rose-400 via-fuchsia-500 to-indigo-500 rounded-full">
-                    <img src="/photoprofil.png" alt="Photo de profil de Alexandre Dissi" className="rounded-full w-52 h-52 md:w-72 md:h-72" />
+                    <img src="/photoprofil.png" alt="Photo de profil de Alexandre Dissi" className="rounded-full w-52 h-52 sm:w-64 sm:w-64 md:w-72 md:h-72 lg:w-80 lg:h-80" />
                 </div>
             </div>
         </div>

8fe617d79dd4ed93d325661642a4c787c762760c
Alexandre Dissi <alexandredissi@icloud.com>
2023-06-05 11:45:32 +0200

    f

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-'use client'
-import React, { useEffect } from 'react';
+"use client";
+import React, { useEffect } from "react";
 import HomePresentation from "./components/home";
 import NavBar from "./components/navbar";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 import AboutMe from "./components/aboutme";
-import Stack from './components/stack';
-import Form from './components/form';
-import Experience from './components/experience';
-import Project from './components/project';
-import Footer from './components/footer';
+import Stack from "./components/stack";
+import Form from "./components/form";
+import Experience from "./components/experience";
+import Project from "./components/project";
+import Footer from "./components/footer";
 
 export default function Home() {
   useEffect(() => {

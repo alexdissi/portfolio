71aa0abd0541835c4a4dc77d8647ad574e7de702
Alexandre Dissi <alexandredissi@icloud.com>
2024-04-02 22:40:23 +0200

    fix: fix responsive design

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -5,7 +5,7 @@ const Avatar = () => {
         <img
         src="/me.png"
         alt="avatar"
-        className={"w-72 h-72 lg:w-[20rem] lg:h-[20rem] xl:w-[25rem] xl:h-[25rem] hover:scale-110 transform transition duration-500 cursor-pointer ease-in-out rounded-full"}
+        className={"w-64 h-64 lg:w-[20rem] lg:h-[20rem] xl:w-[22rem] xl:h-[22rem] hover:scale-110 transform transition duration-500 cursor-pointer ease-in-out rounded-full"}
         />
     );
 }
